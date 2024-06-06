0a6bffeedc45deec890fd71bbe10535b795e0514
sercantopcu <devops.sercan@gmail.com>
2024-06-06 11:45:07 -0500

    Changed tags

--- sec_groups.tf
+++ sec_groups.tf
@@ -23,82 +23,82 @@ resource "aws_security_group" "bean-elb-sg" {
 }
 
 resource "aws_security_group" "bastion-sg" {
-    name        = "aws-terra-bastion-sg"
-    description = "Security group for bastion ec2 instance"
-    vpc_id      = module.vpc.vpc_id
+  name        = "aws-terra-bastion-sg"
+  description = "Security group for bastion ec2 instance"
+  vpc_id      = module.vpc.vpc_id
 
-    ingress {
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = [var.myip]
-    }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.myip]
+  }
 
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "allow_bastion"
-    }
+  tags = {
+    Name = "allow_bastion"
+  }
 }
 
 resource "aws_security_group" "prod-sg" {
-    name        = "aws-terra-prod-sg"
-    description = "Security group for beanstalk instance"
-    vpc_id      = module.vpc.vpc_id
+  name        = "aws-terra-prod-sg"
+  description = "Security group for beanstalk instance"
+  vpc_id      = module.vpc.vpc_id
 
-    ingress {
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        security_groups = [aws_security_group.bastion-sg.id]
-    }
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion-sg.id]
+  }
 
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "allow_beanstalk"
-    }
+  tags = {
+    Name = "allow_beanstalk"
+  }
 }
 
 resource "aws_security_group" "backend-sg" {
-    name        = "aws-terra-backend-sg"
-    description = "Security group for RDS, ActiveMQ, Elastic Cache"
-    vpc_id      = module.vpc.vpc_id
+  name        = "aws-terra-backend-sg"
+  description = "Security group for RDS, ActiveMQ, Elastic Cache"
+  vpc_id      = module.vpc.vpc_id
 
-    ingress {
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        security_groups = [aws_security_group.prod-sg.id]
-    }
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.prod-sg.id]
+  }
 
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "allow_bastion"
-    }
+  tags = {
+    Name = "allow_backend"
+  }
 }
 
 resource "aws_security_group_rule" "sec_group_allow_itself" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 65535
-  protocol          = "tcp"
-  security_group_id = aws_security_group.backend-sg.id
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.backend-sg.id
   source_security_group_id = aws_security_group.backend-sg.id
 }
